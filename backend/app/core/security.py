b4028d4d51d2a49e507cd578eb29adfc6e285e5e
Seyed-Rafie <mrftndrd6@gmail.com>
2026-08-12 08:14:18 +0330

    add hashing password

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta, timezone
-import jwt
+from jose import jwt
+from passlib.context import CryptContext
 from app.core.config import settings
 
 def create_access_token(data: dict) -> str:
@@ -17,3 +18,15 @@ def create_access_token(data: dict) -> str:
         algorithm=settings.ALGORITHM
     )
     return encoded_jwt
+
+# Configuration for password hashing using bcrypt
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    # Generate secure bcrypt hash from raw password
+    print(password)
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    # Verify plain password against hashed password
+    return pwd_context.verify(plain_password, hashed_password)
